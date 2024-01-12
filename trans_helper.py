3768038c30d75c2ddacace040fa8547f2b9bd47e
pkuYmiracle <1771235420@qq.com>
2024-01-12 16:17:28 +0800

    fix trans_helper : add server

--- trans_helper.py
+++ trans_helper.py
@@ -3,6 +3,8 @@ import rsa
 import transFileBits
 import transStrBits
 import random
+PADDING_LEN = 2 ** 8
+
 
 def generate_random_binary_string(length):
     return ''.join(random.choices(['0', '1'], k=length)) 
@@ -54,28 +56,89 @@ def remove_one_and_check(input_str: str) -> (str, bool):
             output_str += input_str[i]
 
     return output_str, flag
+class Server :
+    def __init__(self):
+        self.users = []
 
+    def add_user(self, key_file : str, random_len : int, user_name : str):
+        self.users.append((key_file, random_len, user_name))
+
+    def receive_file(self, file_name : str, all_bits : str):
+        
+        flag = False
+        for i in range(PADDING_LEN):
+            padding_start = ""
+            for j in range(i, PADDING_LEN):
+                padding_start += "0" * (PADDING_LEN) + "{:08b}".format(j)
+            find_index = all_bits.find(padding_start)
+            if find_index != -1:
+                all_bits = all_bits[find_index + len(padding_start):]
+                flag = True
+                break
+        if flag == False:
+            print("No padding start")
+            return
+        all_bits,flag = remove_one_and_check(all_bits)
+        if flag == False:
+            print("Error in format!")
+            return
+        bits_len = int(all_bits[:32], 2)
+        all_bits = all_bits[32:]
+        user_name = transStrBits.trans_bits_to_str(all_bits[:bits_len])
+        all_bits = all_bits[bits_len:]
+        print("user_name :{}, len : {}".format(user_name[:10], bits_len))
+        user = ()
+        for i in self.users:
+            if i[2] == user_name:
+                user = i
+                break
+        
+        with open(user[0] + 'publicKey.pem', 'rb') as p:
+            publicKey = rsa.PublicKey.load_pkcs1(p.read())
+        with open(user[0] + 'privateKey.pem', 'rb') as p:
+            privateKey = rsa.PrivateKey.load_pkcs1(p.read())
+        
+        if user == ():
+            print("No such user!")
+            return
+        password_len = int(all_bits[:32], 2)
+        all_bits = all_bits[32:]
+        password = transStrBits.trans_bits_to_str(all_bits[:password_len])
+        all_bits = all_bits[password_len:]
+        print("password :{}, len : {}".format(password[:10], password_len))
+        if password != rsa_helper.decrypt(user_name, privateKey):
+            print("Wrong password!")
+            return
+        file_len = int(all_bits[:32], 2)
+        all_bits = all_bits[32:]
+        print("file len : {}".format(file_len))
+        all_bits = all_bits[user[1]:]
+        file_bits = all_bits[:file_len]
+        file = transStrBits.trans_bits_to_str(file_bits)
+        file = rsa_helper.decrypt(file, privateKey)
+        with open(file_name, 'w') as f:
+            f.write(file)
+        print("Receive file successfully!")
 
 class Client :
     def __init__(self, key_file : str, random_len : int, user_name : str):
         
         with open(key_file + 'publicKey.pem', 'rb') as p:
-            self.publicKey = rsa.PublicKey.load_pkcs1(p.read())
-        self.padding_len = 2 ** 8
+            self.publicKey = rsa.PublicKey.load_pkcs1(p.read()) 
         self.random_len = random_len
         self.user_name = user_name
         self.password = rsa_helper.encrypt(self.user_name.encode('utf-8'), self.publicKey)
 
     def send_file(self, file_name : str):
         # transfer file to  
-        # padding start + user_name + pubkey(user_name)  + [ file_len + randombits(len = len(user_name)) + file + randombits(len = len(user_name))] + padding end
+        # padding start + user_name + encrypt(user_name)  + file_len + [ randombits(len = len(user_name)) + encrypt(file) + randombits(len = len(user_name))] + padding end
         # before transfer, both client and server know the len and keys
         
         all_bits = ""
         
         padding_start = ""
-        for i in range(self.padding_len):
-            padding_start += "0" * (self.padding_len) + "{:08b}".format(i)
+        for i in range(PADDING_LEN):
+            padding_start += "0" * (PADDING_LEN) + "{:08b}".format(i)
         
         print("padding start :", padding_start)
          
@@ -99,9 +162,9 @@ class Client :
         file_bits = transStrBits.trans_str_to_bits(file_bits) 
         bits_len = len(file_bits)
         print("file len : {}".format(bits_len))
-        all_bits += transStrBits.trans_str_to_bits(rsa_helper.encrypt("{:032b}".format(bits_len),self.publicKey))
+        all_bits += "{:032b}".format(bits_len)
         all_bits += generate_random_binary_string(self.random_len)
-        all_bits += add_one_and_check(file_bits)
+        all_bits += file_bits
         all_bits += generate_random_binary_string(self.random_len)
 
         return padding_start + add_one_and_check(all_bits)
