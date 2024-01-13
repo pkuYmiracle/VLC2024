12d578d23b720a84574070697b3f55a311171167
pkuYmiracle <1771235420@qq.com>
2024-01-13 22:31:33 +0800

    add Bob

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # gen keys
 import rsa_helper
-rsa_helper.generate_keys("keys/Bob/")
+rsa_helper.generate_keys("keys/Bob/",key_len=256)
 import trans_helper
 import random
 client = trans_helper.Client("keys/Bob/",random_len=10,user_name="Bob", password="password")
@@ -19,6 +19,7 @@ for i in range(0, len(send_bits), 16):
     else:
         send_bits = send_bits[:pos] + '0' +  send_bits[pos + 1 : ]
      
-assert(before_bits != send_bits)
+diff = sum([send_bits[i] != before_bits[i] for i in range(len(send_bits))])/ len(send_bits)
+print("diff rate " , diff)
  
 server.receive_file("test2.txt",send_bits)
